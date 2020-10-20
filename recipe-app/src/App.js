fa7d4dd189a76d1fa9d0fab125f69a63d68dd818
coryp4 <cory@burtano.com>
2020-10-20 16:39:40 -0700

    first-commit

---
+++ recipe-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+
+  const APP_ID = "a4328cd7";
+  const APP_KEY = "4d6104d00d42318b680a40aa6c3465f8";
+
+  return(
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
